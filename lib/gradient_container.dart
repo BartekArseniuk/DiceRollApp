f6775a22f7540fc14ca5f57f98ccf8ff04b6fded
Bartosz Arseniuk <bartekarseniuk1701@gmail.com>
2024-06-13 19:43:39 +0200

    Create simple dice roll app

---
+++ lib/gradient_container.dart
@@ -0,0 +1,27 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+var startAligment = Alignment.topLeft;
+var endAligment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAligment,
+          end: endAligment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
